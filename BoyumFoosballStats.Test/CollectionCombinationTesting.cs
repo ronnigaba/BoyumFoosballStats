30e5c84cac6550dd15fbb7b00bf2fa824df1ff9f
ronnigaba <ronni.gaba@gmail.com>
2023-09-14 20:19:01 +0200

    Random AI helper improvements and testing stuff

---
+++ BoyumFoosballStats.Test/CollectionCombinationTesting.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BoyumFoosballStats_Ai;
+using BoyumFoosballStats.Controller;
+using BoyumFoosballStats.Model;
+using BoyumFoosballStats.Model.Enums;
+using Xunit;
+
+namespace BoyumFoosballStats.Test;
+
+public class CollectionCombinationTesting
+{
+    [Fact]
+    public async void TestCombinations()
+    {
+        var eloController = new EloController();
+        var blobHelper = new AzureBlobStorageHelper();
+        var matches = await blobHelper.GetEntries<Match>(AzureBlobStorageHelper.DefaultMatchesFileName);
+        var teamStats = new TeamStatisticsController().CalculateTeamStats(matches);
+        var players = new List<Player>() { Player.Ronni, Player.Jeppe, Player.Alex, Player.Peter };
+        var combinations = CollectionCombinationHelper.GetAllCombinations(players, 2).ToList();
+        var validMatchFairness = new Dictionary<Match, decimal>();
+        var validMatchFairnessAi = new Dictionary<Match, float>();
+
+        foreach (var comb1 in combinations)
+        {
+            foreach (var comb2 in combinations)
+            {
+                var match = new Match
+                {
+                    Black = new Team(TableSide.Black) { Defender = comb1.First(), Attacker = comb1.Last() },
+                    Gray = new Team(TableSide.Black) { Defender = comb2.First(), Attacker = comb2.Last() }
+                };
+                var grayElo =
+                    teamStats?.SingleOrDefault(x => x.TeamIdentifier == match.Gray.TeamIdentifier)?.EloRating ?? 0;
+                var blackElo = teamStats?.SingleOrDefault(x => x.TeamIdentifier == match.Black.TeamIdentifier)
+                    ?.EloRating ?? 0;
+                if (match.IsValid())
+                {
+                    var prediction = eloController.PredictResult(grayElo, blackElo);
+                    validMatchFairness.Add(match, Math.Abs(prediction[0] - prediction[1]));
+
+                    var result = await AiPredictionController.Predict(match);
+                    validMatchFairnessAi.Add(match, result?.WinChanceBlack ?? 0);
+                }
+            }
+        }
+
+        var fairestMatch = validMatchFairness.FirstOrDefault(x => x.Value == validMatchFairness.Min(v => v.Value));
+        var fairestMatchX = validMatchFairnessAi.MinBy(kv => Math.Abs(kv.Value - 50));
+        var test = 0;
+    }
+}
---
+++ BoyumFoosballStats/Controller/AiPredictionController.cs
@@ -0,0 +1,35 @@
+﻿using BoyumFoosballStats_Ai;
+using BoyumFoosballStats.Model;
+using BoyumFoosballStats.Model.Enums;
+
+namespace BoyumFoosballStats.Controller;
+
+public static class AiPredictionController
+{
+    public static async Task<AiMatchPrediction?> Predict(Match match)
+    {
+        if (!match.IsValid())
+        {
+            return null;
+        }
+        var sampleData = new MatchOutcomeModel.ModelInput
+        {
+            GrayDefender = (float)match.Gray.Defender,
+            GrayAttacker = (float)match.Gray.Attacker,
+            BlackDefender = (float)match.Black.Defender,
+            BlackAttacker = (float)match.Black.Attacker,
+        };
+        var outcomeModel = new MatchOutcomeModel();
+        var result = await outcomeModel.Predict(sampleData);
+        var blackChance = 100 - result.Score * 100;
+        var grayChance = 0 + result.Score * 100;
+
+        return new AiMatchPrediction
+        {
+            WinChanceBlack = blackChance,
+            WinChanceGray = grayChance,
+            WinningTeam = blackChance > grayChance ? match.Black : match.Gray,
+            WinningSide = blackChance > grayChance ? TableSide.Black : TableSide.Gray
+        };
+    }
+}
